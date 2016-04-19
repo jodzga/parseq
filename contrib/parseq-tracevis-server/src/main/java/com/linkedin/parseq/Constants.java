b0187d788721efd2e41a992a80cf561db53bbdfc
jodzga <jodzga@linkedin.com>
2016-04-19 10:50:32 -0700

    Improved tests. Reorganized GetRequestGroup.

--- contrib/parseq-tracevis-server/src/main/java/com/linkedin/parseq/Constants.java
+++ contrib/parseq-tracevis-server/src/main/java/com/linkedin/parseq/Constants.java
@@ -5,7 +5,7 @@ public final class Constants {
   private Constants() {}
 
   public static final int DEFAULT_CACHE_SIZE = 1024;
-  public static final long DEFAULT_TIMEOUT_MS = 5000;
+  public static final long DEFAULT_TIMEOUT_MS = 300000;
   public static final String TRACEVIS_SUBDIRECTORY = "tracevis";
   public static final String CACHE_SUBDIRECTORY = "cache";
   public static final String OUTPUT_TYPE = "svg";
